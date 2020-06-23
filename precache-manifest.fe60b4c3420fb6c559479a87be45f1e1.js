1bf76437ade8ba5b9b40621a931466d63732480c
Mateusz Butkiewicz <butkiewicz.mat@gmail.com>
2020-06-23 20:50:00 +0200

    Updates

---
+++ precache-manifest.fe60b4c3420fb6c559479a87be45f1e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ff1f32e2095e1b3fe6d6ac359cde835",
+    "url": "/JS-homework-pong/index.html"
+  },
+  {
+    "revision": "3c136dae23a2a105f9d5",
+    "url": "/JS-homework-pong/static/js/2.c556e612.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/JS-homework-pong/static/js/2.c556e612.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c668f96a9679469c640e",
+    "url": "/JS-homework-pong/static/js/main.811ac24d.chunk.js"
+  },
+  {
+    "revision": "0f581b4b4ea60a36b495",
+    "url": "/JS-homework-pong/static/js/runtime-main.000536fd.js"
+  }
+]);
